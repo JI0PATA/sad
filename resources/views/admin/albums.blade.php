76f12e526cdf5603ddc9b3dc29797f66b8cb83e8
JI0PATA <wanja.shukov14@gmail.com>
2018-04-18 16:20:25 +0300

    Исправлен косяк с отображением на главной альбомов

--- resources/views/admin/albums.blade.php
+++ resources/views/admin/albums.blade.php
@@ -6,15 +6,12 @@
     </div>
     <div class="albums__items wp">
         @foreach($albums as $album)
-            <div class="albums__item">
+            <a href="{{ route('user.photosInAlbum', ['id' => $album['id']]) }}" class="albums__item">
                 <div class="album__title">{{ $album['title'] }}</div>
-                <div class="album__preview">
-                    <img src="{{ asset('img/albums/'.$album['img']) }}" alt="">
+                <div class="album__preview" style="background-image: url({{ asset('img/albums/'.$album['img']) }})">
+                    <div class="view-hidden">посмотреть</div>
                 </div>
-                <a href="{{ route('photosInAlbum', ['id' => $album['id']]) }}">Обзор</a><br>
-                <a href="{{ route('editAlbum', ['id' => $album['id']]) }}">Редактировать</a><br>
-                <a href="{{ route('deleteAlbum', ['id' => $album['id']]) }}">Удалить</a>
-            </div>
+            </a>
         @endforeach
     </div>
 @endsection
