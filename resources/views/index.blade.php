f9bb9d1b29fba17c52f56ddac4f189f596026e14
JI0PATA <wanja.shukov14@gmail.com>
2018-04-18 16:27:07 +0300

    Исправлен косяк с '' в background-image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,7 +84,7 @@
             @foreach($albums as $album)
                 <a href="{{ route('user.photosInAlbum', ['id' => $album['id']]) }}" class="albums__item">
                     <div class="album__title">{{ $album['title'] }}</div>
-                    <div class="album__preview" style="background-image: url({{ asset('img/albums/'.$album['img']) }})">
+                    <div class="album__preview" style="background-image: url('{{ asset('img/albums/'.$album['img']) }}')">
                         <div class="view-hidden">посмотреть</div>
                     </div>
                 </a>
