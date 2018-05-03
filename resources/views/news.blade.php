89010324c250854a5d48a1a5b43b90c89a46bf6a
JI0PATA <wanja.shukov14@gmail.com>
2018-05-03 16:02:32 +0300

    Исправлено отображение новостей

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -10,9 +10,13 @@
                     <div class="news__info">
                         <div class="news__img"
                              style="background-image: url({{ asset('img/news/'.$item['img']) }})"></div>
-                        <div class="news__description">
-                            {!!  $item['description'] !!}
-                            <br><a href="{{ route('article', ['id' => $item['id']]) }}">Читать...</a>
+                        <div>
+                            <div class="news__description">
+                                {!!  $item['description'] !!}
+                            </div>
+                            <div>
+                                <a href="{{ route('article', ['id' => $item['id']]) }}">Читать...</a>
+                            </div>
                         </div>
                     </div>
                     <div class="news__date">{{ $item['created_at']->format('d.m.Y H:i') }}</div>
