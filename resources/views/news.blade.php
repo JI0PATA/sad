a9ca2d445c52794bbb53dea41ab31d1ae85b4539
JI0PATA <wanja.shukov14@gmail.com>
2018-05-21 13:03:23 +0300

    Исправлено отображение картинок в новостях

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -9,7 +9,7 @@
                     <div class="news__title">{{ $item['title'] }}</div>
                     <div class="news__info">
                         <div class="news__img"
-                             style="background-image: url({{ asset('img/news/'.$item['img']) }})"></div>
+                             style="background-image: url('{{ asset('img/news/'.$item['img']) }}')"></div>
                         <div>
                             <div class="news__description">
                                 {!!  $item['description'] !!}
